fb2d422e1fb82b81e718dbb56c1a1b12953185d2
chb-cloud <chenghb1874@163.com>
2024-08-31 16:19:20 +0000

    Update randombg.js Updated By Github Actions

--- js/randombg.js
+++ js/randombg.js
@@ -1,7 +1,12 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
   "url(/img/钓鱼.jpg)",
-  "url(/img/橘子.jpg)"
+  "url(/img/1.jpeg)",
+  "url(/img/2.jpg)",
+  "url(/img/3.jpg)",
+  "url(/img/4.jpg)",
+  "url(/img/5.jpg)",
+  "url(/img/6.jpg)"
   ];
   //获取背景图片总数，生成随机数
   var bgindex =Math.floor(Math.random() * backimg.length);
@@ -10,7 +15,12 @@ var backimg =[
   //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
   var bannerimg =[
   "url(/img/钓鱼.jpg)",
-  "url(/img/橘子.jpg)"
+  "url(/img/1.jpeg)",
+  "url(/img/2.jpg)",
+  "url(/img/3.jpg)",
+  "url(/img/4.jpg)",
+  "url(/img/5.jpg)",
+  "url(/img/6.jpg)"
   ];
   //获取banner图片总数,生成随机数
   var bannerindex =Math.floor(Math.random() * bannerimg.length);
