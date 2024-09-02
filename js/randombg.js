e0d8db8ae3c24222b47f03f0e8861e6bc5560afd
chb-cloud <chenghb1874@163.com>
2024-09-02 15:44:16 +0000

    Create deploy.yml Updated By Github Actions

--- js/randombg.js
+++ js/randombg.js
@@ -1,11 +1,9 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
-  "url(/img/钓鱼.jpg)",
-  "url(/img/1.jpeg)",
-  "url(/img/2.jpg)",
-  "url(/img/3.jpg)",
-  "url(/img/4.jpg)",
-  "url(/img/5.jpg)"
+  "url(/img/bg1.jpg)",
+  "url(/img/橘子.jpg)",
+  "url(/img/海鸥.jpg)",
+  "url(/img/bg8.jpg)"
   ];
   //获取背景图片总数，生成随机数
   var bgindex =Math.floor(Math.random() * backimg.length);
@@ -13,12 +11,10 @@ var backimg =[
   document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
   //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
   var bannerimg =[
-  "url(/img/钓鱼.jpg)",
-  "url(/img/1.jpeg)",
-  "url(/img/2.jpg)",
-  "url(/img/3.jpg)",
-  "url(/img/4.jpg)",
-  "url(/img/5.jpg)"
+    "url(/img/bg1.jpg)",
+    "url(/img/橘子.jpg)",
+    "url(/img/海鸥.jpg)",
+    "url(/img/bg8.jpg)"
   ];
   //获取banner图片总数,生成随机数
   var bannerindex =Math.floor(Math.random() * bannerimg.length);
